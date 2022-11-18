9bed9956319392125930cdd3d3797e9a7b69cc03
Frapier <cedricfrapier@gmail.com>
2022-11-18 15:59:30 +0100

    Mise en place de la nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
+import React from "react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-console.log(MyApp);
+
 export default MyApp;
